dd01f9def2a8a473403a9b606e2f4a890f5f107c
githubuser2007 <shubhisthenewflash@gmail.com>
2022-06-17 14:54:45 +0530

    trying to manage questions

---
+++ quiz-app/app.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+
+alert('To start quiz click on the submit button');
+
+const quiz_data = ['Fullform of HTML','Fullform of CSS','Fullform of JSON,'];
+
+const answers = ['Hyper Text Markup Language','Cascading Style Sheet','JavaScript Object Notation'];
+
+const q_label = document.getElementById('label1');
+
+const input = document.getElementById('input1');
+
+
+
