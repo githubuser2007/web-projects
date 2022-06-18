fad7d7d5f32405a05bb0b08aacf38891ed39bff2
githubuser2007 <shubhisthenewflash@gmail.com>
2022-06-18 14:14:11 +0530

    near to finish the part where questions will show when button clicked

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -1,8 +1,6 @@
-console.log('Hello World');
+// alert('To start quiz click on the submit button');
 
-alert('To start quiz click on the submit button');
-
-const quiz_data = ['Fullform of HTML','Fullform of CSS','Fullform of JSON,'];
+const quiz_data = ['Fullform of HTML','Fullform of CSS','Fullform of JSON'];
 
 const answers = ['Hyper Text Markup Language','Cascading Style Sheet','JavaScript Object Notation'];
 
@@ -10,5 +8,11 @@ const q_label = document.getElementById('label1');
 
 const input = document.getElementById('input1');
 
+const button = document.getElementById('submit');
+
+button.addEventListener('click',() => {
+
+    
 
 
+})
