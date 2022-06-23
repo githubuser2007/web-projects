41f06bf42d5edea0bd1d2d1552e042f7c8ada906
githubuser2007 <shubhisthenewflash@gmail.com>
2022-06-23 20:19:44 +0530

    tried my best

---
+++ practise-fontawesome/js/app.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("nav-toggle")
+
+menu.addEventListener('click',()=>{
+let toggle = document.getElementsByClassName("nav-item")
+toggle.style.display ="block"
+
+})
